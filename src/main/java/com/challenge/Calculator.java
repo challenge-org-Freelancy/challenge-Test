ae2600d203c3b2d424db05b09b198f5631fac566
amanibenzaghdane-pixel <amanibenzaghdane@gmail.com>
2026-01-27 22:02:15 +0100

    Update Calculator.java

--- src/main/java/com/challenge/Calculator.java
+++ src/main/java/com/challenge/Calculator.java
@@ -25,11 +25,20 @@ public class Calculator {
         return a / b;
     }
 
+    // New method: Returns remainder of a divided by b
+    public int modulus(int a, int b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("Division by zero!");
+        }
+        return a % b;
+    }
+
     public static void main(String[] args) {
         Calculator calc = new Calculator();
         System.out.println("Add: " + calc.add(10, 5));
         System.out.println("Subtract: " + calc.subtract(10, 5));
         System.out.println("Multiply: " + calc.multiply(10, 5));
         System.out.println("Divide: " + calc.divide(10, 5));
+        System.out.println("Modulus: " + calc.modulus(10, 5)); // new output
     }
 }
